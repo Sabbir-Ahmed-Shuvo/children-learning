4cb1f92f1552df2f3e130713aacd8497fdbd4c8c
sabbirNew <sabbir.ahmed5936@gmail.com>
2021-10-05 14:13:06 +0600

    add more file

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -7,7 +7,7 @@ const About = () => {
                         <div className="card">
                             <div className="card-body">
                                 <h5 className="card-title">Art Learning Club</h5>
-                                <p className="card-text">With over one quarter of the world speaking English, you don’t have to search very far to find scores of resources and opportunities to practice the language if you’re wanting to learn. With some 750,000 English words and strange ways of spelling that can baffle even those whose primary language is English, the language can definitely be one of the more challenging to master—especially if you’re hoping to do so quickly.</p>
+                                <p className="card-text">The artist Fernand Léger will be viewed, discussed and used as inspiration, especially his circus and carnival artwork from the 1950’s.These works were created on iPads using the art app Art Studio. Firstly, the children took photos of themselves or others in an action pose. These photos were imported into the app and a “layer” created over the top of the image. The children drew on this “layer” until they had completed their drawing.</p>
                             </div>
                         </div>
                     </div>
@@ -15,7 +15,7 @@ const About = () => {
                         <div className="card">
                             <div className="card-body">
                                 <h5 className="card-title">Chess Learning Club</h5>
-                                <p className="card-text">With over one quarter of the world speaking English, you don’t have to search very far to find scores of resources and opportunities to practice the language if you’re wanting to learn. With some 750,000 English words and strange ways of spelling that can baffle even those whose primary language is English, the language can definitely be one of the more challenging to master—especially if you’re hoping to do so quickly.</p>
+                                <p className="card-text">Chess.com Global Learning Club is a club that dedicates itself for teaching chess.com users to improve and enjoy their game. Here at the Chess.com Global Learning Club, we share techniques to make ourselves better at the game. Anyone who joins can post ideas, openings, endgame strategies, or even puzzles so everyone can learn but have fun. Join now! and have fun. By: kamalesh25 (kamalesh) and The EpicChessAttacker (sreyansh)</p>
                             </div>
                         </div>
                     </div>
@@ -23,7 +23,7 @@ const About = () => {
                         <div className="card">
                             <div className="card-body">
                                 <h5 className="card-title">Cooking Learning Club</h5>
-                                <p className="card-text">With over one quarter of the world speaking English, you don’t have to search very far to find scores of resources and opportunities to practice the language if you’re wanting to learn. With some 750,000 English words and strange ways of spelling that can baffle even those whose primary language is English, the language can definitely be one of the more challenging to master—especially if you’re hoping to do so quickly.</p>
+                                <p className="card-text">Cooking is something I really enjoy doing but I don’t talk about it a lot. The DP is a wholesome programme which encourages its students to study, participate in events and take part in service. When I got the chance to lead a club, I took the opportunity and ensured that I lead a club which, I loved teaching. After a long talk with two of my classmates we decided to start “The Cooking Club” or “Health & Nutrition Club”. Our main idea is to teach</p>
                             </div>
                         </div>
                     </div>
@@ -31,7 +31,8 @@ const About = () => {
                         <div className="card">
                             <div className="card-body">
                                 <h5 className="card-title">Guiter Learning Club</h5>
-                                <p className="card-text">With over one quarter of the world speaking English, you don’t have to search very far to find scores of resources and opportunities to practice the language if you’re wanting to learn. With some 750,000 English words and strange ways of spelling that can baffle even those whose primary language is English, the language can definitely be one of the more challenging to master—especially if you’re hoping to do so quickly.</p>
+                                <p className="card-text">The Guitar Social is unique, vibrant, edgy, always fun and uplifting. Where else could I get top quality guitar teaching alongside a jam in the park, fun Zoom guitar based games or an inspiring and sociable camping trip.
+                                The class is run by a group who really understand the principles of learning and the reasons for doing it! Also we are given sensible and realistic targets and outcomes coupled with very helpful practice schedules.</p>
                             </div>
                         </div>
                     </div>
@@ -39,7 +40,7 @@ const About = () => {
                         <div className="card">
                             <div className="card-body">
                                 <h5 className="card-title">Hip Hop Dance Class</h5>
-                                <p className="card-text">With over one quarter of the world speaking English, you don’t have to search very far to find scores of resources and opportunities to practice the language if you’re wanting to learn. With some 750,000 English words and strange ways of spelling that can baffle even those whose primary language is English, the language can definitely be one of the more challenging to master—especially if you’re hoping to do so quickly..</p>
+                                <p className="card-text">Welcome to HIP HOP DANCE JUNKIES!! We are proud to introduce to you our "ICONIX Courses", the first virtual GROUP CLASSES offered by HHDJ.  Much like our normal group classes, please "BOOK NOW" as classes operate on a "FIRST COME, FIRST SERVE" basis. The "ICONIX Courses" will feature choreographed routines (old & new) taught in a virtual grouproutines (old & new) taught in a virtual group...routines (old & new) taught in a virtual</p>
                             </div>
                         </div>
                     </div>
@@ -47,7 +48,7 @@ const About = () => {
                         <div className="card">
                             <div className="card-body">
                                 <h5 className="card-title">Piano Learning Club</h5>
-                                <p className="card-text">With over one quarter of the world speaking English, you don’t have to search very far to find scores of resources and opportunities to practice the language if you’re wanting to learn. With some 750,000 English words and strange ways of spelling that can baffle even those whose primary language is English, the language can definitely be one of the more challenging to master—especially if you’re hoping to do so quickly.</p>
+                                <p className="card-text">. Piano lessons help children perfect their natural learning processes. Music is a language, and children are programmed to absorb languages. Studies have shown that kids can most easily learn new languages when they start at a young age. Piano lessons help children develop the very same skills needed to accomplish language arts, like reading. In piano, students learn how to follow directions, scan written materials, think critically, create</p>
                             </div>
                         </div>
                     </div>
@@ -63,7 +64,7 @@ const About = () => {
                         <div className="card">
                             <div className="card-body">
                                 <h5 className="card-title">Music Production Class</h5>
-                                <p className="card-text">With over one quarter of the world speaking English, you don’t have to search very far to find scores of resources and opportunities to practice the language if you’re wanting to learn. With some 750,000 English words and strange ways of spelling that can baffle even those whose primary language is English, the language can definitely be one of the more challenging to master—especially if you’re hoping to do so quickly.</p>
+                                <p className="card-text">Music Production Course, Sound Engineering Courses, DJ, Live Sound and more Entertainment Education Programs are now much at your reach. Learn from the Safety of your HOME. PATSAV’s LIVE ONLINE Classes Offer Innovative Learning & at Attractive Course Fees with Flexible Installment options. “Music Gives Soul To The Universe, Wings To The Mind, Flight To The Imagination & Life To Everything – Plato”.</p>
                             </div>
                         </div>
                     </div>
@@ -71,7 +72,7 @@ const About = () => {
                         <div className="card">
                             <div className="card-body">
                                 <h5 className="card-title">Cooking Club</h5>
-                                <p className="card-text">With over one quarter of the world speaking English, you don’t have to search very far to find scores of resources and opportunities to practice the language if you’re wanting to learn. With some 750,000 English words and strange ways of spelling that can baffle even those whose primary language is English, the language can definitely be one of the more challenging to master—especially if you’re hoping to do so quickly.</p>
+                                <p className="card-text">Cooking is something I really enjoy doing but I don’t talk about it a lot. The DP is a wholesome programme which encourages its students to study, participate in events and take part in service. When I got the chance to lead a club, I took the opportunity and ensured that I lead a club which, I loved teaching. After a long talk with two of my classmates we decided to start “The Cooking Club” or “Health & Nutrition Club”.</p>
                             </div>
                         </div>
                     </div>
--- src/component/Detials/Detials.js
+++ src/component/Detials/Detials.js
@@ -1,16 +1,47 @@
 import React from 'react';
-import img from'../../img/how-do-clubs-work-header.jpg'
+import img from'../../img/how-do-clubs-work-header.jpg';
+import image1 from'../../img/GettyImages-56901647-581032823df78c2c7349a8a0.webp';
+import image2 from'../../img/GettyImages-584304124-581032cb3df78c2c7349b374.webp';
+import image3 from'../../img/GettyImages-463029317-581033455f9b58564c9918cd.webp';
+import image4 from'../../img/GettyImages-533978201-581033c95f9b58564c992794.webp';
+import image5 from'../../img/GettyImages-200548159-003-581034de5f9b58564c99611a.webp';
+
 const Detials = () => {
     return (
-        <div className="container mt-4">
+        <div className="container mt-4 w-50">
             <div class="card mb-3">
                 <img src={img} class="card-img-top" alt="..."/>
                 <div class="card-body">
-                    <h5 class="card-title">Our Club Details</h5>
+                    <h3 class="card-title">Our Club Details</h3>
                     <p class="card-text">Groups and clubs designed for children and youth offer many benefits. They help kids make friends and practice social skills with both adults and peers. They allow to children to learn new skills or pursue a favorite hobby or activity. They foster feelings of competence and pride to boost kids' confidence and self-esteem.
                     Some children join clubs to serve the community or explore their faith. Others prefer the spirit of competition that comes from youth sports. When you are choosing a club for your child, consider what your child hopes to get out of the experience and the various types of organizations available. Here is a quick list to give you some direction and help you find the right club or team for your child.</p>
                 </div>
-                
+                <h3>Volunteering</h3>
+                <img src={image1} class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <p class="card-text">Children learn a lot about themselves and others by volunteering. Even the youngest of children can make a difference. By choosing a project and following it through, children develop a sense of accomplishment.1 Whether planting flowers or serving in a soup kitchen, children see the results of their hard work and have the satisfaction of knowing they are making a difference.</p>
+                </div>
+                <h3>Scouting</h3>
+                <img src={image2} class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <p class="card-text">Scouting provides a well-rounded program where children earn badges, play games, camp, sing, create crafts, complete service projects and more. Scouts build friendships and learn new skills.2﻿ Many have opportunities through Scouts that they wouldn't otherwise have.</p>
+                </div>
+                <h3>Sports</h3>
+                <img src={image3} class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <p class="card-text">Sports give children a chance to engage in physical activity and make friends, develop skills, learn teamwork and build self-esteem.3 The sporting options are virtually endless.</p>
+                </div>
+                <h3>Academic</h3>
+                <img src={image4} class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <p class="card-text">Academic clubs recognize children's academic achievements and give children the opportunity to work in an academic area in more depth than offered in class. Groups that focus on math, science, and computers encourage leadership skills while helping children learn to work as a team. They are a place for kids to meet like-minded friends who share their passion for learning.</p>
+                </div>
+                <h3>Music & Arts</h3>
+                <img src={image5} class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <p class="card-text">Children develop creativity and express themselves through the arts.5 Singing, dancing, painting, and drama give children the chance to find out who they are — or sometimes pretend to be someone different — and express it in beautiful ways.
+                    Contact local music schools and art studios for information on youth groups and programs. Libraries are another great resource for creative camps and programs geared for kids. For the child who loves reading, consider forming a kids' book club where children take turns choosing a favorite book to read and discuss together.</p>
+                </div>
             </div>
         </div>
     );
